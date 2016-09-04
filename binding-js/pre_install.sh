c0739e7b218982c57c0a792999c4a0fb6f782cf4
root <smith.jessk@gmail.com>
2016-09-04 23:17:51 +0000

    Fix errors in build scripts

--- binding-js/pre_install.sh
+++ binding-js/pre_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-# If gulp does not exist, install it
+# If these dependencies do not exist, install them
 hash gulp 2>/dev/null || npm i -g gulp
-hash tsfmt 2>/dev/null || npm i -g tsfmt
+hash tsfmt 2>/dev/null || npm i -g typescript-formatter
+hash typings 2>/dev/null || npm i -g typings
 
 (cd src && typings install)
