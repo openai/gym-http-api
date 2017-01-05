cf90ff3ce18aa5408b08f889d61843de20c93f9f
Daniel Lucsanszky <dan.lucsanszky@gmail.com>
2017-01-05 01:41:36 +0100

    added post request for v1/envs/:instance_id/reset/ endpoint, added tests

--- binding-hs/src/Lib.hs
+++ binding-hs/src/Lib.hs
@@ -1,11 +1,20 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TypeOperators              #-}
 
 module Lib
-    ( run
-    ) where
+  ( run
+  , envCreate
+  , envListAll
+  , envReset
+  , EnvID (..)
+  , InstID (..)
+  , Observation (..)
+  , Environment (..)
+  , gymAPI
+  ) where
 
 import           Control.Monad.Trans.Except (ExceptT, runExceptT)
 import           Data.Aeson
@@ -17,28 +26,48 @@ import           Network.HTTP.Client        (Manager, defaultManagerSettings,
                                              newManager)
 import           Servant.API
 import           Servant.Client
-
-newtype Environment = Environment { all_envs :: Object }
-  deriving (Show, Generic)
-
-instance ToJSON Environment
-instance FromJSON Environment
+import           Test.QuickCheck.Arbitrary
+import           Test.QuickCheck.Instances
 
 newtype EnvID = EnvID { env_id :: T.Text }
   deriving Generic
 
 instance ToJSON EnvID
+instance FromJSON EnvID
 
-type GymAPI = "v1" :> "envs" :> ReqBody '[JSON] EnvID :> Post '[JSON] Object
-         :<|> "v1" :> "envs" :> Get '[JSON] Environment
+newtype InstID = InstID { instance_id :: T.Text }
+  deriving (Eq, Show, Arbitrary, Generic)
+
+instance ToJSON InstID
+instance FromJSON InstID
+
+newtype Environment = Environment { all_envs :: HM.HashMap T.Text T.Text }
+  deriving (Eq, Show, Arbitrary, Generic)
+
+instance ToJSON Environment
+instance FromJSON Environment
+
+newtype Observation = Observation { observation :: [Double] }
+  deriving (Eq, Show, Arbitrary, Generic)
+
+instance ToJSON Observation
+instance FromJSON Observation
+
+type GymAPI =
+  "v1" :> "envs" :>
+    ( ReqBody '[JSON] EnvID :> Post '[JSON] InstID
+ :<|> Get '[JSON] Environment
+ :<|> Capture "instance_id" T.Text :> "reset" :> Post '[JSON] Observation
+    )
 
 gymAPI :: Proxy GymAPI
 gymAPI = Proxy
 
-envCreate :: EnvID -> Manager -> BaseUrl -> ExceptT ServantError IO Object
+envCreate :: EnvID -> Manager -> BaseUrl -> ExceptT ServantError IO InstID
 envListAll :: Manager -> BaseUrl -> ExceptT ServantError IO Environment
+envReset :: T.Text -> Manager -> BaseUrl -> ExceptT ServantError IO Observation
 
-envCreate :<|> envListAll = client gymAPI
+envCreate :<|> envListAll :<|> envReset = client gymAPI
 
 run :: IO ()
 run = do
@@ -50,8 +79,13 @@ run = do
 
   case inst of
     Left err -> print err
-    Right ok -> print $ encode ok
+    Right ok@InstID {instance_id = id} -> do
+      print ok
+      obs <- runExceptT $ envReset id manager url
+      case obs of
+        Left err -> print err
+        Right o  -> print o
 
   case res of
     Left err  -> putStrLn $ "Error: " ++ show err
-    Right env -> print $ encode env
+    Right env -> print env
