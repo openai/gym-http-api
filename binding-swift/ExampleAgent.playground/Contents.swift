88986063b9b35d5163524389e6a8e31ca82eb9c6
andrewschreiber <andrew.schreiber1@gmail.com>
2017-02-22 20:16:48 -0800

    feat: convert callbacks to semaphores

--- binding-swift/ExampleAgent.playground/Contents.swift
+++ binding-swift/ExampleAgent.playground/Contents.swift
@@ -2,63 +2,28 @@
 import PlaygroundSupport
 import Foundation
 
-PlaygroundPage.current.needsIndefiniteExecution = true
+//PlaygroundPage.current.needsIndefiniteExecution = true
 
 let client = GymClient(baseURL: URL(string:"http://localhost:5000")!)
+let existing = client.listAll()
 
-//client.listAll { instanceEnvMap in
-////    print("Got map: \(instanceEnvMap)")
-//    if let instanceID = instanceEnvMap.first?.key{
-////        client.startMonitor(instanceID: instanceID, directory: "/tmp/cartpole-monitor", force: false, resume: false, videoCallable: false)
-//        
-////        client.close(instanceID: instanceID)
-////        client.closeMonitor(instanceID: instanceID)
-//        client.actionSpace(instanceID: instanceID, callback: { (space) in
-//            print("Got space succeeded")
-//        })
-//        client.actionSpace(instanceID: instanceID, callback: { (space) in
-//            print("Got space succeeded")
-//        })
-////        client.step(instanceID: instanceID, action: Action(base:[5.0] as AnyObject), callback: { (step) in
-////            print("Got step result \(step)")
-////        })
-//    }
-//}
+let id:InstanceID = client.create(envID: "CartPole-v0")
 
-client.create(envID: "CartPole-v0") { (instanceID) in
-    print("Create succeeded: \(instanceID)")
-//    defer { client.close(instanceID: instanceID) }
-//
-//    print("Got instance ID: \(instanceID)")
-//    
-    client.observationSpace(instanceID: instanceID) { (space) in
-        print(space)
-    }
-    client.actionSpace(instanceID: instanceID) { (space) in
-        print(space)
-    }
-    client.startMonitor(instanceID: instanceID, directory: "/tmp/cartpole-monitor", force: true, resume: false, videoCallable: false) {
-        client.reset(instanceID: instanceID) { (obs) in
-            var stepping = true
-            var count = 0
-    
-            print("Starting repeat")
-            repeat {
-                count += 1
-                client.sampleAction(instanceID: instanceID) { (action) in
-                    print("got action: \(action)")
+client.startMonitor(instanceID: id, directory: "/tmp/swift-example-agent", force: true, resume: false, videoCallable: false)
 
-                    
-                    client.step(instanceID: instanceID, action: action) { (result) in
-                        print(result)
-                        if result.done {
-                            stepping = false
-                        }
-                    }
-                }
-                
-            } while(stepping && count < 10000)
-            
-        }
+let obs = client.reset(instanceID: id)
+
+print("First observation: \(obs)")
+
+while true {
+    let action = client.sampleAction(instanceID: id)
+    let result = client.step(instanceID: id, action: action)
+    if result.done {
+        print(result)
+        break
     }
 }
+
+client.closeMonitor(instanceID: id)
+client.uploadResults(directory: "/tmp/swift-example-agent", apiKey: nil, algorithmID: nil)
+
--- binding-swift/ExampleAgent.playground/Sources/GymClient.swift
+++ binding-swift/ExampleAgent.playground/Sources/GymClient.swift
@@ -7,95 +7,80 @@ open class GymClient {
         self.baseURL = baseURL
     }
     
-    open func listAll(callback:@escaping ([InstanceID:EnvID]) -> Void) {
-        get(url: baseURL.appendingPathComponent("/v1/envs/")) { (json) in
-            let map = (json as! [String:[InstanceID:EnvID]])["all_envs"]!
-            callback(map)
-        }
+    open func listAll() -> [InstanceID:EnvID] {
+        let json = get(url: baseURL.appendingPathComponent("/v1/envs/"))
+        let map = (json as! [String:[InstanceID:EnvID]])["all_envs"]!
+        return map
     }
     
-    open func create(envID:EnvID, callback:@escaping (InstanceID) -> Void) {
-        post(url: baseURL.appendingPathComponent("/v1/envs/"),
-             parameter: ["env_id":envID]) { (json) in
-                let instanceID = (json as! [String:InstanceID])["instance_id"]!
-                callback(instanceID)
-        }
+    open func create(envID:EnvID) -> InstanceID{
+        let json = post(url: baseURL.appendingPathComponent("/v1/envs/"),
+                        parameter: ["env_id":envID])
+        let instanceID = (json as! [String:InstanceID])["instance_id"]!
+        return instanceID
     }
     
-    open func reset(instanceID:InstanceID, callback:@escaping (Observation) -> Void) {
-        post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/reset/")) { (json) in
-            let obs = (json as! [String:AnyObject])["observation"]!
-            callback(Observation(base: obs))
-        }
+    open func reset(instanceID:InstanceID) -> Observation {
+        let json = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/reset/"))
+        let obs = (json as! [String:AnyObject])["observation"]!
+        return Observation(base: obs)
     }
     
-    open func step(instanceID:InstanceID, action:Action, render:Bool = false, callback:@escaping (StepResult) -> Void) {
+    open func step(instanceID:InstanceID, action:Action, render:Bool = false) -> StepResult {
         let parameter = ["action":action.base, "render":render] as [String : Any]
-        post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/step/"), parameter: parameter) { (json) in
-            let result = StepResult(jsonDict: json as! [String:AnyObject])
-            callback(result)
-        }
+        let json = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/step/"),
+                        parameter: parameter)
+        let result = StepResult(jsonDict: json as! [String:AnyObject])
+        return result
     }
     
-    open func actionSpace(instanceID:InstanceID, callback:@escaping (Space) -> Void) {
-        getSpace(instanceID: instanceID, name: "action_space", callback: callback)
+    open func actionSpace(instanceID:InstanceID) -> Space {
+        return getSpace(instanceID: instanceID, name: "action_space")
     }
     
-    open func observationSpace(instanceID:InstanceID, callback:@escaping (Space) -> Void) {
-        getSpace(instanceID: instanceID, name: "observation_space", callback: callback)
+    open func observationSpace(instanceID:InstanceID) -> Space {
+        return getSpace(instanceID: instanceID, name: "observation_space")
     }
     
-    private func getSpace(instanceID:InstanceID, name:String, callback:@escaping (Space) -> Void) {
-        get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/\(name)/")) { (json) in
-            let dict = (json as! [String:AnyObject])["info"] as! [String:AnyObject]
-            let space = Space(jsonDict: dict)
-            callback(space)
-        }
-    }
-    open func sampleAction(instanceID:InstanceID, callback:@escaping (Action) -> Void) {
-        get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/action_space/sample")) { (json) in
-            let action = (json as! [String:AnyObject])["action"]!
-            callback(Action(base:action))
-        }
+    private func getSpace(instanceID:InstanceID, name:String) -> Space {
+        let json = get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/\(name)/"))
+        let dict = (json as! [String:AnyObject])["info"] as! [String:AnyObject]
+        return Space(jsonDict: dict)
     }
     
-    open func containsAction(instanceID:InstanceID, action:Action, callback:@escaping (Bool) -> Void) {
+    open func sampleAction(instanceID:InstanceID) -> Action {
+        let json = get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/action_space/sample"))
+        let action = (json as! [String:AnyObject])["action"]!
+        return Action(base:action)
+    }
+    
+    open func containsAction(instanceID:InstanceID, action:Action) -> Bool {
         guard action.discreteValue != nil else { fatalError("Currently only int action types are supported") }
-        get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/action_space/contains/")) { (json) in
-            let member = (json as! [String:Bool])["member"]!
-            callback(member)
-        }
+        let json = get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/action_space/contains/"))
+        let member = (json as! [String:Bool])["member"]!
+        return member
     }
     
     open func close(instanceID:InstanceID) {
-        post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/close/")) { (json) in
-            print("Result of close: \(json)")
-        }
+        _ = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/close/"))
     }
     
-    open func startMonitor(instanceID:InstanceID, directory:String, force:Bool, resume:Bool, videoCallable:Bool, callback:@escaping (Void) -> Void) {
+    open func startMonitor(instanceID:InstanceID, directory:String, force:Bool, resume:Bool, videoCallable:Bool) {
     
         let parameter = ["directory":directory,
                          "force": force,
                          "resume":resume,
                          "video_callable": videoCallable] as [String : Any]
     
-        post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/monitor/start/"), parameter: parameter) { (json) in
-            print("Result of start monitor \(json)")
-            callback()
-        }
+        _ = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/monitor/start/"), parameter: parameter)
     }
     
     open func closeMonitor(instanceID:InstanceID) {
-        post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/monitor/close/")) { (json) in
-            print("Result of close monitor \(json)")
-        }
+       _ = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/monitor/close/"))
     }
     
     open func shutdown() {
-        post(url: baseURL.appendingPathComponent("/v1/shutdown/")) { (json) in
-            print("Result of shutdown \(json)")
-        }
+        _ = post(url: baseURL.appendingPathComponent("/v1/shutdown/"))
     }
     
     open func uploadResults(directory:String, apiKey:String?, algorithmID:String?) {
@@ -106,24 +91,26 @@ open class GymClient {
             data["algorithm_id"] = algorithmID
         }
         
-        post(url: baseURL.appendingPathComponent("/v1/upload/"), parameter: data) { (json) in
-            print("Result of upload \(json)")
-        }
+        _ = post(url: baseURL.appendingPathComponent("/v1/upload/"), parameter: data)
     }
     
     // MARK: Helpers
     
-    private func get(url:URL, parameter:Any? = nil, callback:@escaping (Any?) -> Void) {
+    private func get(url:URL, parameter:Any? = nil) -> Any? {
+        var json:Any?
+        let semaphore = DispatchSemaphore(value: 0)
         let task = URLSession.shared.dataTask(with: url) { (data, res, error) in
             self.httpErrorHandler(data: data, res: res, error: error)
             
-            let json = try! JSONSerialization.jsonObject(with: data!, options: [.allowFragments])
-            callback(json)
+            json = try! JSONSerialization.jsonObject(with: data!, options: [.allowFragments])
+            semaphore.signal()
         }
         task.resume()
+        _ = semaphore.wait(timeout: .distantFuture)
+        return json
     }
     
-    private func post(url:URL, parameter:Any? = nil, callback:@escaping (Any?) -> Void) {
+    private func post(url:URL, parameter:Any? = nil) -> Any? {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -132,13 +119,21 @@ open class GymClient {
         if let parameter = parameter {
             request.httpBody = try! JSONSerialization.data(withJSONObject: parameter, options: [])
         }
+        var json:Any?
+        let semaphore = DispatchSemaphore(value: 0)
+
         let task = URLSession.shared.dataTask(with:request) { (data, res, error) in
             self.httpErrorHandler(data: data, res: res, error: error)
             
-            let json = try? JSONSerialization.jsonObject(with: data!, options: [.allowFragments])
-            callback(json ?? [:])
+            json = try? JSONSerialization.jsonObject(with: data!, options: [.allowFragments])
+            semaphore.signal()
+            
         }
         task.resume()
+        
+        _ = semaphore.wait(timeout: .distantFuture)
+        
+        return json
     }
     
     private func httpErrorHandler(data:Data?, res:URLResponse?, error:Error?) {
