0c5b3e64842c392e9ed28fa1e468bbaecf2d725e
andrewschreiber <andrew.schreiber1@gmail.com>
2017-02-22 21:35:52 -0800

    feat: documentation

--- binding-swift/ExampleAgent.playground/Contents.swift
+++ binding-swift/ExampleAgent.playground/Contents.swift
@@ -1,24 +1,32 @@
 import Foundation
 
-let client = GymClient(baseURL: URL(string:"http://localhost:5000")!)
-let existing = client.listAll()
+let baseDirectory = "/tmp/swift-example-agent"
 
+// Create a client
+let client = GymClient(baseURL: URL(string:"http://localhost:5000")!)
+
+// Make a new environment with the CartPole task
 let id = client.create(envID: "CartPole-v0")
 
+// Verify action and observation space
 print(client.actionSpace(instanceID: id))
 print(client.observationSpace(instanceID: id))
 
-client.startMonitor(instanceID: id, directory: "/tmp/swift-example-agent", force: true, resume: false, videoCallable: false)
+// Start recording, and wipe out old recordings
+client.startMonitor(instanceID: id, directory: baseDirectory, force: true, resume: false, videoCallable: false)
 
+// Refresh to get our first observation
 let obs = client.reset(instanceID: id)
-
 print("First observation: \(obs)")
 
 var count = 0
 while true {
+    // Find a random action purely as a demonstration. Replace with an action chosen by your algorithm.
     let action = client.sampleAction(instanceID: id)
-
+    
+    // Execute the action in the environment
     let result = client.step(instanceID: id, action: action)
+    
     print("Result on iteration \(count). \nReward: \(result.reward). Observation: \(result.observation).")
     count += 1
     if result.done {
@@ -26,9 +34,10 @@ while true {
     }
 }
 
+// Clean up
 client.closeMonitor(instanceID: id)
 client.close(instanceID: id)
 
 // Get your api key from https://gym.openai.com/users/{your_name}
-// client.uploadResults(directory: "/tmp/swift-example-agent", apiKey: nil, algorithmID: nil)
+// client.uploadResults(directory: baseDirectory, apiKey: nil, algorithmID: nil)
 
--- binding-swift/ExampleAgent.playground/Sources/GymClient.swift
+++ binding-swift/ExampleAgent.playground/Sources/GymClient.swift
@@ -1,30 +1,54 @@
+//
+// GymClient.swift
+// GYM-HTTP-API
+// Created by Andrew Schreiber on 2/2/17.
+//
+
+// Add this file to your iOS/MacOS project to access a Gym HTTP server
+
 import Foundation
 
 open class GymClient {
-    let baseURL:URL
+    /// The URL for the Gym HTTP server
+    public let baseURL:URL
     
+    /// Creates an instance for interfacing with the Gym HTTP client.
     public init(baseURL:URL) {
         self.baseURL = baseURL
     }
     
-    open func listAll() -> [InstanceID:EnvID] {
-        let json = get(url: baseURL.appendingPathComponent("/v1/envs/"))
-        let map = (json as! [String:[InstanceID:EnvID]])["all_envs"]!
-        return map
-    }
-    
-    open func create(envID:EnvID) -> InstanceID{
+    /// Create a new environment with a gym environment ID string i.e. "CartPole-v0"
+    /// - returns : An instanceID to be used to uniquely identify the environment to be manipulated
+    open func create(envID:EnvID) -> InstanceID {
         let json = post(url: baseURL.appendingPathComponent("/v1/envs/"),
                         parameter: ["env_id":envID])
         let instanceID = (json as! [String:InstanceID])["instance_id"]!
         return instanceID
     }
     
+    /// Get all existing environment instances. The list is reset each time the server is reset.
+    /// - returns : A dictionary of instanceIDs and the gym environment ID they are made from
+    open func listAll() -> [InstanceID:EnvID] {
+        let json = get(url: baseURL.appendingPathComponent("/v1/envs/"))
+        let map = (json as! [String:[InstanceID:EnvID]])["all_envs"]!
+        return map
+    }
+    
+    /// Reset the state of the environment
+    /// The resulting observation type may vary.
+    /// For discrete spaces, it is an Int.
+    /// For vector spaces, it is a [Double].
+    /// - returns : An initial observation
     open func reset(instanceID:InstanceID) -> Observation {
         let json = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/reset/"))
         let obs = (json as! [String:AnyObject])["observation"]!
         return Observation(base: obs)
     }
+    
+    /// Run one timestep of the environment's dynamics.
+    /// - parameter action : An action to take. For discrete spaces, it should be an Int. For vector spaces, it should be a [Double].
+    /// - parameter render : Undocumented functionality XD
+    /// - returns : StepResult includes an observation of the current environment, amount of reward after the action, if the simulation is done, and any meta data
     
     open func step(instanceID:InstanceID, action:Action, render:Bool = false) -> StepResult {
         let parameter = ["action":action.base, "render":render] as [String : Any]
@@ -34,37 +58,40 @@ open class GymClient {
         return result
     }
     
-    open func actionSpace(instanceID:InstanceID) -> Space {
-        return getSpace(instanceID: instanceID, name: "action_space")
-    }
-    
+    /// Get information (name and dimensions/bounds) of the env's observation_space
     open func observationSpace(instanceID:InstanceID) -> Space {
         return getSpace(instanceID: instanceID, name: "observation_space")
     }
     
-    private func getSpace(instanceID:InstanceID, name:String) -> Space {
-        let json = get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/\(name)/"))
-        let dict = (json as! [String:AnyObject])["info"] as! [String:AnyObject]
-        return Space(jsonDict: dict)
+    /// Get information (name and dimensions/bounds) of the env's action_space
+    open func actionSpace(instanceID:InstanceID) -> Space {
+        return getSpace(instanceID: instanceID, name: "action_space")
     }
     
+    /// Sample an action randomly from all possible actions in the environment
     open func sampleAction(instanceID:InstanceID) -> Action {
         let json = get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/action_space/sample"))
         let action = (json as! [String:AnyObject])["action"]!
         return Action(base:action)
     }
     
+    /// Checks if an action is contained in the action space. Currently, only int action types are supported
     open func containsAction(instanceID:InstanceID, action:Action) -> Bool {
         guard action.discreteValue != nil else { fatalError("Currently only int action types are supported") }
         let json = get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/action_space/contains/\(action.discreteValue!)"))
-        let member = (json as! [String:Bool])["member"]!
-        return member
+        let isMember = (json as! [String:Bool])["member"]!
+        return isMember
     }
     
+    /// Close the environment instance. Must be done before upload.
     open func close(instanceID:InstanceID) {
         _ = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/close/"))
     }
     
+    /// Start recording.
+    /// - parameter directory : Location to write files. The server will create the directory if it does not exist.
+    /// - parameter force : Clear out existing training data from this directory (by deleting every file prefixed with "openaigym.")
+    /// - parameter resume : Retain the training data already in this directory, which will be merged with our new data
     open func startMonitor(instanceID:InstanceID, directory:String, force:Bool, resume:Bool, videoCallable:Bool) {
     
         let parameter = ["directory":directory,
@@ -75,15 +102,23 @@ open class GymClient {
         _ = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/monitor/start/"), parameter: parameter)
     }
     
+    /// Stop recording and flush all data to disk.
+    /// Two files will be created a meta data file like "openaigym.manifest.5.40273.manifest.json" and a performance file like "openaigym.episode_batch.11.40273.stats.json"
     open func closeMonitor(instanceID:InstanceID) {
        _ = post(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/monitor/close/"))
     }
     
+    /// Shut down the server
     open func shutdown() {
         _ = post(url: baseURL.appendingPathComponent("/v1/shutdown/"))
     }
     
-    open func uploadResults(directory:String, apiKey:String?, algorithmID:String?) {
+    /// Upload the results of training (as automatically recorded by your env's monitor) to OpenAI Gym.
+    /// - parameter directory : Absolute path of directory containing recorder files i.e. "/tmp/swift-gym-agent"
+    /// - parameter apiKey : Unique key from openai.com on your account page. Can be ignored if already contained in the environment.
+    /// - parameter algorithmID : A unique identifer for the algorithm. You can safely leave this nil and it will be autogenerated.
+
+    open func uploadResults(directory:String, apiKey:String?, algorithmID:String? = nil) {
         guard let apiKey = apiKey ?? environmentVariable(key:"OPENAI_GYM_API_KEY") else { fatalError("No API Key") }
         
         var data:[String:String] = ["training_dir": directory, "api_key": apiKey]
@@ -144,11 +179,17 @@ open class GymClient {
             fatalError("Error with request:\(text). Response: \(res)")
         }
     }
+    
     private func environmentVariable(key:String) -> String? {
         guard let rawValue = getenv(key) else { return nil }
         return String(utf8String: rawValue)
     }
     
+    private func getSpace(instanceID:InstanceID, name:String) -> Space {
+        let json = get(url: baseURL.appendingPathComponent("/v1/envs/\(instanceID)/\(name)/"))
+        let dict = (json as! [String:AnyObject])["info"] as! [String:AnyObject]
+        return Space(jsonDict: dict)
+    }
 }
 
 // MARK: Models
@@ -198,7 +239,6 @@ public struct MultiValueType {
             print("Unsupported value type: \(base)")
         }
     }
-    
     public var vectorValue:[Double]? {
         return base as? [Double]
     }
