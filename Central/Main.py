f8143af4a6f1ae7110ae4ea62f61664c1c61f15a
nazaruka <48185453+nazaruka@users.noreply.github.com>
2019-05-29 16:15:14 -0500

    Rudimentary parser finished!
    This one works with the aforementioned discrete algorithms and domains. Still trying to clean a few things up and integrate the rest

--- Central/Main.py
+++ Central/Main.py
@@ -9,41 +9,41 @@ import DQN
 import PG
 
 import gym_cmd
-parser = build_parser()
+parser = gym_cmd.build_parser()
 options = parser.parse_args()
 
 if __name__ == '__main__':
 
 	# Assume command line param will be 'CartPole-v1' or 'MountainCar-v1', etc.
+	algorithm = options.algorithm
 	domain = options.domain
 
 	env = gym.make(domain)
 	env.seed(1)
 	env = env.unwrapped
-ys.argv[
-	if(s2] == "DQN"):
+
+	if(algorithm == "DQN"):
 		agent = DQN.DQN(act_dim=env.action_space.n, obs_dim=env.observation_space.shape[0],
-					lr_q_value=options.q_val, gamma=options.gamma, epsilon=options.epsilon)
-	elif(sys.argv[2] == "A2C"):
+					lr_q_value=0.02, gamma=options.gamma, epsilon=options.epsilon)
+	elif(algorithm == "A2C"):
 		agent = A2C.ActorCritic(act_dim=env.action_space.n, obs_dim=env.observation_space.shape[0],
-					lr_actor=0.01, lr_value=0.02, gamma=0.99)
-	elif(sys.argv[2] == "PG"):
+					lr_actor=0.01, lr_value=0.02, gamma=options.gamma)
+	elif(algorithm == "PG"):
 		agent = PG.PolicyGradient(act_dim=env.action_space.n, obs_dim=env.observation_space.shape[0], 
-					lr=0.02, gamma=0.99)
+					lr=0.02, gamma=options.gamma)
 
 	else:
 		print("Invalid algorithm specified. Only A2C, DQN, and PG currently supported")
 		quit()
 				
-	nepisode = 1000
+	nepisode = options.nepisode
 	batch_size = 128
 	iteration = 0
-	nstep = 200
 	
 	# Necessary for DQN
-	epsilon_step = 10
-	epsilon_decay = 0.99
-	epsilon_min = 0.001
+	epsilon_step = options.epsilon_step
+	epsilon_decay = options.epsilon_decay
+	epsilon_min = options.epsilon_min
 	
 	for i_episode in range(nepisode):
 		obs0 = env.reset()
@@ -64,10 +64,10 @@ ys.argv[
 
 			# The following code can probably be cleaned up in a more general way
 			if isinstance(agent, DQN.DQN):
-				if iteration >= 128 * 3:
+				if iteration >= batch_size * 3:
 					agent.learn() 
 					if iteration % epsilon_step == 0:
-						agent.epsilon = max([agent.epsilon * 0.99, 0.001])
+						agent.epsilon = max([agent.epsilon * epsilon_decay, epsilon_min])
 	
 			if done:
 				# Try to find a cleaner way of doing this
--- Central/gym_cmd.py
+++ Central/gym_cmd.py
@@ -1,5 +1,37 @@
 from argparse import ArgumentParser
 
+# default arguments
+EPSILON = 0.2
+EPSILON_STEP = 10
+EPSILON_DECAY = 0.99
+EPSILON_MIN = 0.001
+GAMMA = 0.999
+NEPISODE = 1000
+
 def build_parser():
     parser = ArgumentParser()
-    parser.add_argument()
+    parser.add_argument('algorithm', metavar='ALG', help='The algorithm you wish to ' + \
+                'work with (currently only A2C, DQN, and PG).')
+    parser.add_argument('domain', metavar='DOMAIN', help='The OpenAI Gym domain you ' + \
+                'wish to work in (currently only discrete).')
+    parser.add_argument('-e', '--eps', type=float,
+            dest='epsilon', help='DQN: epsilon parameter (default %(default)s)',
+            metavar='EPSILON', default=EPSILON)
+    parser.add_argument('--ep_s', type=float,
+            dest='epsilon_step', help='DQN: epsilon step (default %(default)s)',
+            metavar='EPSILON_STEP', default=EPSILON_STEP)
+    parser.add_argument('--ep_d', type=float,
+            dest='epsilon_decay', help='DQN: epsilon decay (default %(default)s)',
+            metavar='EPSILON_DECAY', default=EPSILON_DECAY)
+    parser.add_argument('--ep_m', type=float,
+            dest='epsilon_min', help='DQN: minimum epsilon (default %(default)s)',
+            metavar='EPSILON_MIN', default=EPSILON_MIN)
+    parser.add_argument('-g', '--gamma', type=float,
+            dest='gamma', help='Gamma parameter (default %(default)s)',
+            metavar='GAMMA', default=GAMMA)
+    parser.add_argument('--nepisode', type=int,
+            dest='nepisode', help='Number of episodes (default %(default)s)',
+            metavar='NEPISODE', default=NEPISODE)
+    return parser
+
+#def lrq = 0.02, gamma = 0.999, epsilon = 0.2, estep = 10, edecay = 0.99, emin = 0.001
