0f95064e60ec0c152907fa6dde45a926551a27b0
nazaruka <48185453+nazaruka@users.noreply.github.com>
2019-05-30 11:33:43 -0500

    Update gym_cmd.py

--- Central/gym_cmd.py
+++ Central/gym_cmd.py
@@ -10,28 +10,28 @@ NEPISODE = 1000
 
 def build_parser():
     parser = ArgumentParser()
-    parser.add_argument('algorithm', metavar='ALG', help='The algorithm you wish to ' + \
-                'work with (currently only A2C, DQN, and PG).')
-    parser.add_argument('domain', metavar='DOMAIN', help='The OpenAI Gym domain you ' + \
-                'wish to work in (currently only discrete).')
-    parser.add_argument('-e', '--eps', type=float,
+    parser.add_argument('-a', dest='algorithm', help='The algorithm you wish to work '  + \
+                'with (currently only A2C, DQN, and PG).', metavar='ALG', required=True)
+    parser.add_argument('-d', dest='domain', help='The OpenAI Gym domain you wish to '  + \
+                'work in (currently only discrete).', metavar='DOMAIN', required=True)
+    parser.add_argument('-e', type=float,
             dest='epsilon', help='DQN: epsilon parameter (default %(default)s)',
             metavar='EPSILON', default=EPSILON)
-    parser.add_argument('--ep_s', type=float,
+    parser.add_argument('--estep', type=float,
             dest='epsilon_step', help='DQN: epsilon step (default %(default)s)',
             metavar='EPSILON_STEP', default=EPSILON_STEP)
-    parser.add_argument('--ep_d', type=float,
+    parser.add_argument('--edecay', type=float,
             dest='epsilon_decay', help='DQN: epsilon decay (default %(default)s)',
             metavar='EPSILON_DECAY', default=EPSILON_DECAY)
-    parser.add_argument('--ep_m', type=float,
+    parser.add_argument('--emin', type=float,
             dest='epsilon_min', help='DQN: minimum epsilon (default %(default)s)',
             metavar='EPSILON_MIN', default=EPSILON_MIN)
-    parser.add_argument('-g', '--gamma', type=float,
+    parser.add_argument('-g', type=float,
             dest='gamma', help='Gamma parameter (default %(default)s)',
             metavar='GAMMA', default=GAMMA)
     parser.add_argument('--nepisode', type=int,
             dest='nepisode', help='Number of episodes (default %(default)s)',
-            metavar='NEPISODE', default=NEPISODE)
+            choices=range(1000), metavar='NEPISODE', default=NEPISODE)
     return parser
 
 #def lrq = 0.02, gamma = 0.999, epsilon = 0.2, estep = 10, edecay = 0.99, emin = 0.001
