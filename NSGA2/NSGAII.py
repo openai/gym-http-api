2eb876c4f69e24b26b0e9224beed9aa10b94d37d
Jacob <schrum2@southwestern.edu>
2019-06-19 14:14:36 -0500

    Added main check and indented

--- NSGA2/NSGAII.py
+++ NSGA2/NSGAII.py
@@ -99,57 +99,58 @@ def mutation(solution):
         solution = min_x+(max_x-min_x)*random.random()
     return solution
 
-#Main program starts here
-pop_size = 20
-max_gen = 921
+if __name__ == '__main__':
+    #Main program starts here
+    pop_size = 20
+    max_gen = 921
 
-#Initialization
-min_x=-55
-max_x=55
-solution=[min_x+(max_x-min_x)*random.random() for i in range(0,pop_size)]
-gen_no=0
-while(gen_no<max_gen):
-    function1_values = [function1(solution[i])for i in range(0,pop_size)]
-    function2_values = [function2(solution[i])for i in range(0,pop_size)]
-    non_dominated_sorted_solution = fast_non_dominated_sort(function1_values[:],function2_values[:])
-    print("The best front for Generation number ",gen_no, " is")
-    for valuez in non_dominated_sorted_solution[0]:
-        print(round(solution[valuez],3),end=" ")
-    print("\n")
-    crowding_distance_values=[]
-    for i in range(0,len(non_dominated_sorted_solution)):
-        crowding_distance_values.append(crowding_distance(function1_values[:],function2_values[:],non_dominated_sorted_solution[i][:]))
-    solution2 = solution[:]
-    #Generating offsprings
-    while(len(solution2)!=2*pop_size):
-        a1 = random.randint(0,pop_size-1)
-        b1 = random.randint(0,pop_size-1)
-        solution2.append(crossover(solution[a1],solution[b1]))
-    function1_values2 = [function1(solution2[i])for i in range(0,2*pop_size)]
-    function2_values2 = [function2(solution2[i])for i in range(0,2*pop_size)]
-    non_dominated_sorted_solution2 = fast_non_dominated_sort(function1_values2[:],function2_values2[:])
-    crowding_distance_values2=[]
-    for i in range(0,len(non_dominated_sorted_solution2)):
-        crowding_distance_values2.append(crowding_distance(function1_values2[:],function2_values2[:],non_dominated_sorted_solution2[i][:]))
-    new_solution= []
-    for i in range(0,len(non_dominated_sorted_solution2)):
-        non_dominated_sorted_solution2_1 = [index_of(non_dominated_sorted_solution2[i][j],non_dominated_sorted_solution2[i] ) for j in range(0,len(non_dominated_sorted_solution2[i]))]
-        front22 = sort_by_values(non_dominated_sorted_solution2_1[:], crowding_distance_values2[i][:])
-        front = [non_dominated_sorted_solution2[i][front22[j]] for j in range(0,len(non_dominated_sorted_solution2[i]))]
-        front.reverse()
-        for value in front:
-            new_solution.append(value)
-            if(len(new_solution)==pop_size):
+    #Initialization
+    min_x=-55
+    max_x=55
+    solution=[min_x+(max_x-min_x)*random.random() for i in range(0,pop_size)]
+    gen_no=0
+    while(gen_no<max_gen):
+        function1_values = [function1(solution[i])for i in range(0,pop_size)]
+        function2_values = [function2(solution[i])for i in range(0,pop_size)]
+        non_dominated_sorted_solution = fast_non_dominated_sort(function1_values[:],function2_values[:])
+        print("The best front for Generation number ",gen_no, " is")
+        for valuez in non_dominated_sorted_solution[0]:
+            print(round(solution[valuez],3),end=" ")
+        print("\n")
+        crowding_distance_values=[]
+        for i in range(0,len(non_dominated_sorted_solution)):
+            crowding_distance_values.append(crowding_distance(function1_values[:],function2_values[:],non_dominated_sorted_solution[i][:]))
+        solution2 = solution[:]
+        #Generating offsprings
+        while(len(solution2)!=2*pop_size):
+            a1 = random.randint(0,pop_size-1)
+            b1 = random.randint(0,pop_size-1)
+            solution2.append(crossover(solution[a1],solution[b1]))
+        function1_values2 = [function1(solution2[i])for i in range(0,2*pop_size)]
+        function2_values2 = [function2(solution2[i])for i in range(0,2*pop_size)]
+        non_dominated_sorted_solution2 = fast_non_dominated_sort(function1_values2[:],function2_values2[:])
+        crowding_distance_values2=[]
+        for i in range(0,len(non_dominated_sorted_solution2)):
+            crowding_distance_values2.append(crowding_distance(function1_values2[:],function2_values2[:],non_dominated_sorted_solution2[i][:]))
+        new_solution= []
+        for i in range(0,len(non_dominated_sorted_solution2)):
+            non_dominated_sorted_solution2_1 = [index_of(non_dominated_sorted_solution2[i][j],non_dominated_sorted_solution2[i] ) for j in range(0,len(non_dominated_sorted_solution2[i]))]
+            front22 = sort_by_values(non_dominated_sorted_solution2_1[:], crowding_distance_values2[i][:])
+            front = [non_dominated_sorted_solution2[i][front22[j]] for j in range(0,len(non_dominated_sorted_solution2[i]))]
+            front.reverse()
+            for value in front:
+                new_solution.append(value)
+                if(len(new_solution)==pop_size):
+                    break
+            if (len(new_solution) == pop_size):
                 break
-        if (len(new_solution) == pop_size):
-            break
-    solution = [solution2[i] for i in new_solution]
-    gen_no = gen_no + 1
+        solution = [solution2[i] for i in new_solution]
+        gen_no = gen_no + 1
 
-#Lets plot the final front now
-function1 = [i * -1 for i in function1_values]
-function2 = [j * -1 for j in function2_values]
-plt.xlabel('Function 1', fontsize=15)
-plt.ylabel('Function 2', fontsize=15)
-plt.scatter(function1, function2)
-plt.show()
+    #Lets plot the final front now
+    function1 = [i * -1 for i in function1_values]
+    function2 = [j * -1 for j in function2_values]
+    plt.xlabel('Function 1', fontsize=15)
+    plt.ylabel('Function 2', fontsize=15)
+    plt.scatter(function1, function2)
+    plt.show()
