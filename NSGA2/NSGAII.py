2a1f12e260685585f4aab86141553fc27fdac21b
nazaruka <48185453+nazaruka@users.noreply.github.com>
2019-06-26 16:53:41 -0500

    Update NSGAII.py

--- NSGA2/NSGAII.py
+++ NSGA2/NSGAII.py
@@ -3,7 +3,7 @@
 # Author: Haris Ali Khan 
 # Supervisor: Prof. Manoj Kumar Tiwari
 
-#Importing required modules
+# Importing required modules
 import math
 import random
 import matplotlib.pyplot as plt
@@ -29,51 +29,54 @@ from helpers.storage import RolloutStorage
 # Use the Sonic contest environment instead
 from retro_contest.local import make
 
-#Function to find index of list
+
+# Function to find index of list
 def index_of(a,list):
     for i in range(0,len(list)):
         if list[i] == a:
             return i
     return -1
-    
-#Function to sort by values
+
+
+# Function to sort by values
 def sort_by_values(list1, values):
     sorted_list = []
-    while(len(sorted_list)!=len(list1)):
-        if index_of(min(values),values) in list1:
-            sorted_list.append(index_of(min(values),values))
-        values[index_of(min(values),values)] = math.inf
+    while len(sorted_list) != len(list1):
+        if index_of(min(values), values) in list1:
+            sorted_list.append(index_of(min(values), values))
+        values[index_of(min(values), values)] = math.inf
     return sorted_list
 
-#Function to carry out NSGA-II's fast non dominated sort
+
+# Function to carry out NSGA-II's fast non dominated sort
 def fast_non_dominated_sort(values1, values2):
-    S=[[] for i in range(0,len(values1))]
+    S = [[] for i in range(0, len(values1))]
     front = [[]]
-    n=[0 for i in range(0,len(values1))]
+    n = [0 for i in range(0, len(values1))]
     rank = [0 for i in range(0, len(values1))]
 
     for p in range(0,len(values1)):
-        S[p]=[]
-        n[p]=0
+        S[p] = []
+        n[p] = 0
         for q in range(0, len(values1)):
             if (values1[p] > values1[q] and values2[p] > values2[q]) or (values1[p] >= values1[q] and values2[p] > values2[q]) or (values1[p] > values1[q] and values2[p] >= values2[q]):
                 if q not in S[p]:
                     S[p].append(q)
             elif (values1[q] > values1[p] and values2[q] > values2[p]) or (values1[q] >= values1[p] and values2[q] > values2[p]) or (values1[q] > values1[p] and values2[q] >= values2[p]):
                 n[p] = n[p] + 1
-        if n[p]==0:
+        if n[p] == 0:
             rank[p] = 0
             if p not in front[0]:
                 front[0].append(p)
 
     i = 0
-    while(front[i] != []):
-        Q=[]
+    while front[i] != []:
+        Q = []
         for p in front[i]:
             for q in S[p]:
-                n[q] =n[q] - 1
-                if( n[q]==0):
-                    rank[q]=i+1
+                n[q] = n[q] - 1
+                if n[q] == 0:
+                    rank[q] = i+1
                     if q not in Q:
                         Q.append(q)
         i = i+1
@@ -82,12 +85,13 @@ def fast_non_dominated_sort(values1, values2):
     del front[len(front)-1]
     return front
 
+
 def calculate_novelty(behavior_characterizations):
     ns = []
     for i in range(0, len(behavior_characterizations)):
         total_dist = 0
         for j in range(0, len(behavior_characterizations)):
-            if(i != j):
+            if i != j:
                 i_behavior = behavior_characterizations[i]
                 j_behavior = behavior_characterizations[j]
                 dist = euclidean_dist(i_behavior, j_behavior)
@@ -95,6 +99,7 @@ def calculate_novelty(behavior_characterizations):
         avg_dist = total_dist / len(behavior_characterizations)
         ns.append(avg_dist)
     return ns
+
 
 def euclidean_dist(i_behavior, j_behavior):
     index = total = 0
@@ -110,8 +115,9 @@ def euclidean_dist(i_behavior, j_behavior):
         index += 1
     total = math.sqrt(total)
     return total
-    
-#Function to calculate crowding distance
+
+
+# Function to calculate crowding distance
 def crowding_distance(values1, values2, front):
     distance = [0 for i in range(0,len(front))]
     sorted1 = sort_by_values(front, values1[:])
@@ -124,23 +130,26 @@ def crowding_distance(values1, values2, front):
         distance[k] = distance[k]+ (values1[sorted2[k+1]] - values2[sorted2[k-1]])/(max(values2)-min(values2))
     return distance
 
-#Function to carry out the crossover
-def crossover(a,b):
-    r=random.random()
-    if r>0.5:
+
+# Function to carry out the crossover
+def crossover(a, b):
+    r = random.random()
+    if r > 0.5:
         return mutation((a+b)/2)
     else:
         return mutation((a-b)/2)
 
-#Function to carry out the mutation operator
+
+# Function to carry out the mutation operator
 def mutation(solution):
     mutation_prob = random.random()
-    if mutation_prob <1:
+    if mutation_prob < 1:
         solution = min_x+(max_x-min_x)*random.random()
     return solution
 
+
 # Copied from Training.py in the sonicNEAT repo
-def evaluate(env,net,actor_critic):
+def evaluate(env, net, actor_critic):
     current_max_fitness = 0
     fitness_current = 0
     frame = 0
@@ -151,7 +160,7 @@ def evaluate(env,net,actor_critic):
 
     learning_rate = 2.5e-4
     epsilon = 1e-5
-    
+
     agent = ppo.PPO(
             actor_critic,
             clip_param=0.1,
@@ -162,6 +171,11 @@ def evaluate(env,net,actor_critic):
             lr=learning_rate,
             eps=epsilon,
             max_grad_norm=0.5)
+
+    for i in range(len(agent.actor_critic.base.main)):
+        print(agent.actor_critic.base.main[i])
+    for p in agent.actor_critic.base.main.parameters():
+        print(p.size())
 
     num_steps = 128
     num_processes = 1
@@ -175,8 +189,9 @@ def evaluate(env,net,actor_critic):
     num_updates = 50
     for j in range(num_updates):
 
-        #if use_linear_lr_decay:
+        # if use_linear_lr_decay:
         # decrease learning rate linearly
+        # simply changes learning rate of optimizer
         utils.update_linear_schedule(
             agent.optimizer, j, num_updates,
             learning_rate)
@@ -189,7 +204,6 @@ def evaluate(env,net,actor_critic):
                     rollouts.masks[step])
 
             # Schrum: Uncomment this out to watch Sonic as he learns. This should only be done with 1 process though.
-            envs.render()
             # Obser reward and next obs
             obs, reward, done, infos = envs.step(action)
             fitness_current += reward
@@ -198,7 +212,7 @@ def evaluate(env,net,actor_critic):
             ypos = info['y']
             behaviors.append(xpos)
             behaviors.append(ypos)
-            #print(fitness_current)
+            # print(fitness_current)
             
             if done: break
 
@@ -237,21 +251,23 @@ def evaluate(env,net,actor_critic):
 
         if done or counter == 250:
             done = True
-            #print(fitness_current)
+            # print(fitness_current)
     
     # Add code to return the behavior characterization as well.
     return fitness_current, behaviors
+
 
 def random_genome(n):
     # n is the number of weights
     return np.random.rand(1,n)
 
+
 if __name__ == '__main__':
-    #Main program starts here
+    # Main program starts here
 
     # Competition version of environment make:
     # Mus pip install gym==0.12.1 in order for this to work
-    #env = make(game = "SonicTheHedgehog-Genesis", state = "GreenHillZone.Act1")
+    # env = make(game = "SonicTheHedgehog-Genesis", state = "GreenHillZone.Act1")
 
     # Copied from Training.py in the sonicNEAT repo
     imgarray = []
@@ -272,11 +288,12 @@ if __name__ == '__main__':
     # Schrum: Makes these small to test at first
     max_gen = 1
 
-    #Initialization
+    # Initialization
     # Schrum: This will need to be replaced with initialization for the network weights ... probably from -1 to 1, but how many will you need? Depends on network architecture.
     num_weights = 10 # What should this actually be?
-    gen_no=0
-    while(gen_no<max_gen):
+    # solution=[random_genome(num_weights) for i in range(0,pop_size)]
+    gen_no = 0
+    while gen_no < max_gen:
         fitness_scores = []
         behavior_characterizations = []
         # Copied/Adapted from Training.py in the sonicNEAT repo
@@ -290,17 +307,17 @@ if __name__ == '__main__':
             
         # Schrum: Here is where you have to compare all of the behavior characterizations to get the diversity/novelty scores.
         novelty_scores = calculate_novelty(behavior_characterizations)
-        #print(novelty_scores)
+        # print(novelty_scores)
         
         function1_values = fitness_scores
         function2_values = novelty_scores
 
         # Display the fitness scores and novelty scores for debugging
-        #for i in range(0,len(function1_values)):
-        #    print("Fitness:",fitness_scores[i])
-        #    print("Novelty:",novelty_scores[i])
-        #    print("------------------") 
-        #print("+++++++++++++++++++++++++++++++++++++++++")
+        # for i in range(0,len(function1_values)):
+        #     print("Fitness:",fitness_scores[i])
+        #     print("Novelty:",novelty_scores[i])
+        #     print("------------------")
+        # print("+++++++++++++++++++++++++++++++++++++++++")
         
         non_dominated_sorted_solution = fast_non_dominated_sort(function1_values[:],function2_values[:])
         print("The best front for Generation number ",gen_no, " is")
@@ -310,7 +327,7 @@ if __name__ == '__main__':
             print("------------------")
          
          
-        crowding_distance_values=[]
+        crowding_distance_values = []
         for i in range(0,len(non_dominated_sorted_solution)):
             crowding_distance_values.append(crowding_distance(function1_values[:],function2_values[:],non_dominated_sorted_solution[i][:]))
             
@@ -320,33 +337,33 @@ if __name__ == '__main__':
         #         we switch to using CNNs, we will probably want this code (and have to change the code above).
             
         solution2 = solution[:]
-        #Generating offsprings
-        while(len(solution2)!=2*pop_size):
-            a1 = random.randint(0,pop_size-1)
-            b1 = random.randint(0,pop_size-1)
+        # Generating offsprings
+        while len(solution2) != 2*pop_size:
+            a1 = random.randint(0, pop_size-1)
+            b1 = random.randint(0, pop_size-1)
             solution2.append(crossover(solution[a1],solution[b1]))
-        function1_values2 = [function1(solution2[i])for i in range(0,2*pop_size)]
-        function2_values2 = [function2(solution2[i])for i in range(0,2*pop_size)]
-        non_dominated_sorted_solution2 = fast_non_dominated_sort(function1_values2[:],function2_values2[:])
-        crowding_distance_values2=[]
-        for i in range(0,len(non_dominated_sorted_solution2)):
-            crowding_distance_values2.append(crowding_distance(function1_values2[:],function2_values2[:],non_dominated_sorted_solution2[i][:]))
-        new_solution= []
-        for i in range(0,len(non_dominated_sorted_solution2)):
-            non_dominated_sorted_solution2_1 = [index_of(non_dominated_sorted_solution2[i][j],non_dominated_sorted_solution2[i] ) for j in range(0,len(non_dominated_sorted_solution2[i]))]
+        function1_values2 = [function1(solution2[i])for i in range(0, 2*pop_size)]
+        function2_values2 = [function2(solution2[i])for i in range(0, 2*pop_size)]
+        non_dominated_sorted_solution2 = fast_non_dominated_sort(function1_values2[:], function2_values2[:])
+        crowding_distance_values2 = []
+        for i in range(0, len(non_dominated_sorted_solution2)):
+            crowding_distance_values2.append(crowding_distance(function1_values2[:], function2_values2[:], non_dominated_sorted_solution2[i][:]))
+        new_solution = []
+        for i in range(0, len(non_dominated_sorted_solution2)):
+            non_dominated_sorted_solution2_1 = [index_of(non_dominated_sorted_solution2[i][j], non_dominated_sorted_solution2[i] ) for j in range(0,len(non_dominated_sorted_solution2[i]))]
             front22 = sort_by_values(non_dominated_sorted_solution2_1[:], crowding_distance_values2[i][:])
             front = [non_dominated_sorted_solution2[i][front22[j]] for j in range(0,len(non_dominated_sorted_solution2[i]))]
             front.reverse()
             for value in front:
                 new_solution.append(value)
-                if(len(new_solution)==pop_size):
+                if len(new_solution) == pop_size:
                     break
-            if (len(new_solution) == pop_size):
+            if len(new_solution) == pop_size:
                 break
         solution = [solution2[i] for i in new_solution]
         gen_no = gen_no + 1
 
-    #Lets plot the final front now
+    # Let's plot the final front now
     function1 = [i * -1 for i in function1_values]
     function2 = [j * -1 for j in function2_values]
     plt.xlabel('Function 1', fontsize=15)
