dbdf1244a2d2f9a49cd8b596109ff5f736aaa873
nazaruka <48185453+nazaruka@users.noreply.github.com>
2019-06-28 20:38:01 -0500

    Wrapper - work in progress

--- pytorch-a2c-ppo-acktr-gail/a2c_ppo_acktr/envs.py
+++ pytorch-a2c-ppo-acktr-gail/a2c_ppo_acktr/envs.py
@@ -6,7 +6,7 @@ import torch
 from gym.spaces.box import Box
 
 from baselines import bench
-from baselines.common.atari_wrappers import make_atari, wrap_deepmind
+from baselines.common.atari_wrappers import make_atari, wrap_deepmind, FrameStack
 from baselines.common.vec_env import VecEnvWrapper
 from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 from baselines.common.vec_env.shmem_vec_env import ShmemVecEnv
@@ -32,6 +32,72 @@ try:
 except ImportError:
     pass
 
+class SonicDiscretizer(gym.ActionWrapper):
+    """
+    Wrap a gym-retro environment and make it use discrete
+    actions for the Sonic game.
+    """
+    def __init__(self, env):
+        super(SonicDiscretizer, self).__init__(env)
+        buttons = ["B", "A", "MODE", "START", "UP", "DOWN", "LEFT", "RIGHT", "C", "Y", "X", "Z"]
+        actions = [['LEFT'], ['RIGHT'], ['LEFT', 'DOWN'], ['RIGHT', 'DOWN'], ['DOWN'],
+                   ['DOWN', 'B'], ['B']]
+        self._actions = []
+        for action in actions:
+            arr = np.array([False] * 12)
+            for button in action:
+                arr[buttons.index(button)] = True
+            self._actions.append(arr)
+        self.action_space = gym.spaces.Discrete(len(self._actions))
+
+    def action(self, a): # pylint: disable=W0221
+        return self._actions[a].copy()
+
+class RewardScaler(gym.RewardWrapper):
+    """
+    Bring rewards to a reasonable scale for PPO.
+
+    This is incredibly important and effects performance
+    drastically.
+    """
+    def reward(self, reward):
+        return reward * 0.01
+
+class AllowBacktracking(gym.Wrapper):
+    """
+    Use deltas in max(X) as the reward, rather than deltas
+    in X. This way, agents are not discouraged too heavily
+    from exploring backwards if there is no way to advance
+    head-on in the level.
+    """
+    def __init__(self, env):
+        super(AllowBacktracking, self).__init__(env)
+        self._cur_x = 0
+        self._max_x = 0
+
+    def reset(self, **kwargs): # pylint: disable=E0202
+        self._cur_x = 0
+        self._max_x = 0
+        return self.env.reset(**kwargs)
+
+    def step(self, action): # pylint: disable=E0202
+        obs, rew, done, info = self.env.step(action)
+        xpos = info['x']
+        ypos = info['y']
+        self._cur_x += rew
+        rew = max(0, self._cur_x - self._max_x)
+        self._max_x = max(self._max_x, self._cur_x)
+        return obs, rew, done, info
+
+def wrap_sonic(env, stack=True, scale_rew=True):
+    # env = SonicDiscretizer(env)
+    if scale_rew:
+        env = RewardScaler(env)
+    # env = WarpFrame96(env)
+    if stack:
+        env = FrameStack(env, 4)
+    # env = AllowBacktracking(env)
+    return env
 
 def make_env(env_id, seed, rank, log_dir, allow_early_resets):
     def _thunk():
@@ -46,6 +112,7 @@ def make_env(env_id, seed, rank, log_dir, allow_early_resets):
             # Could just replace env_id with "SonicTheHedgehog-Genesis"
             # Provide way of setting the state from the command line?
             env = make(game = env_id, state = "GreenHillZone.Act1")
+            # env = wrap_sonic(env)
         else:
             env = gym.make(env_id)
 
