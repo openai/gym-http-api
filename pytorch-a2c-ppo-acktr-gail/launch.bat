75c3470a7cfb0ce6b19cabd62aea584e852c9ee6
Jacob Schrum <schrum2@southwestern.edu>
2019-07-11 00:13:22 -0500

    Monitor for early resets
    In order to wrap the environment in the Monitor that allows early resets, a log_dir has to be specified.

--- pytorch-a2c-ppo-acktr-gail/launch.bat
+++ pytorch-a2c-ppo-acktr-gail/launch.bat
@@ -7,4 +7,4 @@ REM python main.py --env-name %env% --algo ppo --use-gae --lr 2.5e-4 --clip-para
 
 REM Settings from https://github.com/aborghi/retro_contest_agent/blob/master/metalearner/ppo2ttifrutti_agent.py
 REM num-steps halved from 2048 to 1024 because of limited CUDA video memory
-python main.py --env-name %env% --algo ppo --use-gae --lr 2e-4 --clip-param 0.1 --value-loss-coef 0.5 --num-processes %num_proc% --num-steps 1024 --num-mini-batch 16 --log-interval 1 --use-linear-lr-decay --entropy-coef 0.01 --save-dir "save" --save-interval 5 --gamma 0.99 --gae-lambda 0.95 --ppo-epoch 4 --eval-interval 5
+python main.py --env-name %env% --algo ppo --use-gae --lr 2e-4 --clip-param 0.1 --value-loss-coef 0.5 --num-processes %num_proc% --num-steps 1024 --num-mini-batch 16 --log-interval 1 --use-linear-lr-decay --entropy-coef 0.01 --save-dir "save" --save-interval 5 --gamma 0.99 --gae-lambda 0.95 --ppo-epoch 4 --eval-interval 5 --log-dir "/tmp/gym/"
